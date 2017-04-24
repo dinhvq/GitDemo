bd7b0986ad2a2e81bf7e2f57f4eae02bc5f34122
allvalk <dinhquangvinh92@gmail.com>
2017-04-24 23:35:49 +0700

    demo

--- GitDemo/Program.cs
+++ GitDemo/Program.cs
@@ -10,8 +10,8 @@ namespace GitDemo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("abc");
-        
+            Console.WriteLine("abc2222");
+            
         }
     }
 }
